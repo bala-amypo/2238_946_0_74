434f9e8a5d0f69f65bdfec7f3b80f92dc9d02159
bala-amypo <bala@amypo.in>
2025-12-17 09:38:12 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -1,25 +1,31 @@
 package com.example.demo.controller;
 
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import com.example.demo.entity.Student;
+import com.example.demo.entity.StudentEntity;
 import com.example.demo.service.StudentService;
 
 @RestController
+@RequestMapping("/students")
 public class StudentController {
-    @Autowired
-    StudentService ser;
 
-    @PostMapping("/adddata")
-    public Student createData(@RequestBody Student stu){
+    private final StudentService ser;
+
+    @Autowired
+    public StudentController(StudentService ser) {
+        this.ser = ser;
+    }
+
+    @PostMapping("/add")
+    public StudentEntity createData(@RequestBody StudentEntity stu) {
         return ser.createData(stu);
     }
 
-
-    @GetMapping("/fetchrecord")
-    public List<Student> fetchRecord(){
+    @GetMapping("/fetch")
+    public List<StudentEntity> fetchRecord() {
         return ser.fetchRecord();
     }
 }
--- src/main/java/com/example/demo/service/Impl/StudentService.java
+++ src/main/java/com/example/demo/service/Impl/StudentService.java
@@ -1,23 +1,9 @@
 package com.example.demo.service;
 
 import java.util.List;
-import org. springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.example.demo.entity.Student;
-import com.example.demo.repository. StudentRepository;
 
-@Service
-public class StudentService {
-
-@Autowired
-StudentRepository repo;
-
-//create (or) Insert data
-public Student createData(Student stu) {
-return repo.save(stu);
-
-// Fetch all records
-public List<Student> fetchRecord() {
-return repo.finHA11();
-
+public interface StudentService{
+    Student createData(Student stu);
+    List<Student> fetchRecord();
+}
