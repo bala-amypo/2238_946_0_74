0795a58109cd70ae7ee8aa6b637e4e2ce7a06fc6
bala-amypo <bala@amypo.in>
2025-12-19 08:00:42 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -1,66 +1,70 @@
-package com.example.demo.controller;
+// package com.example.demo.controller;
 
-import java.util.*;
+// import java.util.*;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.web.bind.annotation.*;
+// import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.entity.Student;
-import com.example.demo.service.StudentService;
+// import com.example.demo.entity.Student;
+// import com.example.demo.service.StudentService;
 
-@RestController
-public class StudentController {
+// @RestController
+// public class StudentController {
     
-    @Autowired
-    StudentService ser;
+//     @Autowired
+//     StudentService ser;
     
-    @PostMapping("/adddata")
-    public Student createData(@RequestBody Student stu){
-        return ser.createData(stu);
-    }
+//     @PostMapping("/adddata")
+//     public Student createData(@RequestBody Student stu){
+//         return ser.createData(stu);
+//     }
 
-    @GetMapping("/fetche")
-    public List<Student> fetchRecord(){
-        return ser.fetchRecord();
-    }
+//     @GetMapping("/fetche")
+//     public List<Student> fetchRecord(){
+//         return ser.fetchRecord();
+//     }
 
-    @GetMapping("/fetchdatabyid/{id}")
-    public Optional<Student> fetchDataById(@PathVariable int id){
-        return ser.fetchDataById(id);
+//     @GetMapping("/fetchdatabyid/{id}")
+//     public Optional<Student> fetchDataById(@PathVariable int id){
+//         return ser.fetchDataById(id);
 
-    }
+//     }
 
-    @PutMapping("/updatedata/{id}")
-    public String updateDataById(@PathVariable int id,@RequestBody Student stu){
-    Optional<Student> student = ser.fetchDataById(id);
+//     @PutMapping("/updatedata/{id}")
+//     public String updateDataById(@PathVariable int id,@RequestBody Student stu){
+//     Optional<Student> student = ser.fetchDataById(id);
 
-    if(student.isPresent()){
-     stu.setId(id);
-     ser.createData( stu);
+//     if(student.isPresent()){
+//      stu.setId(id);
+//      ser.createData( stu);
 
-     return "Data Updated Successfully";
-    }
-    else{
+//      return "Data Updated Successfully";
+//     }
+//     else{
 
-        return id+ "not found";
-    }
+//         return id+ "not found";
+//     }
 
-    }
+//     }
 
-    @DeleteMapping("/deletedata/{id}")
-    public String deleteDataById(@PathVariable int id){
-    Optional<Student> student = ser.fetchDataById(id);
-    if(student.isPresent()){
-     ser.deleteData(id);
-     return "Data deleted Successfully";
-    }
-    else{
-        return id + "not found";
-    }
+//     @DeleteMapping("/deletedata/{id}")
+//     public String deleteDataById(@PathVariable int id){
+//     Optional<Student> student = ser.fetchDataById(id);
+//     if(student.isPresent()){
+//      ser.deleteData(id);
+//      return "Data deleted Successfully";
+//     }
+//     else{
+//         return id + "not found";
+//     }
 
 
 
-    }
+//     }
 
-}
+// }
+
+
+Remove-Item -Force "C:\path\to\database.db"
+Remove-Item -Force "C:\path\to\*.mv.db"
